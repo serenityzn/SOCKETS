88e4c3bf46d13fca9f239a8f0c33a7766a8183b2
Volodymyr Lapada <Volodymyr.Lapada@tomtom.com>
2016-08-26 14:04:59 +0200

    fixed client close connection error

--- server.py
+++ server.py
@@ -9,33 +9,41 @@ all_sockets = []
 
 def communication(clsock, clip):
     res = 0
-#    clsock.send("Test socket server >")
     message = clsock.recv(1024)
-    #clsock.send(message + "\n")
-    print "<-RQUEST === [ " + str(message) + " ]"
-    clsock.send("HTTP/1.0 200 OK\nServer: Test Web\nCache-Control: private\nContent-Type: text/html; charset=UTF-8\nLocation: http://127.0.0.1:1234\nContent-Length: 13\nDate: Thu, 25 Aug 2016 16:48:09 GMT\nConnection: close\n\nHello World!\n")
-    print "->RESPOND " + str(flag)
+    if  message: 
+        clsock.send("Test socket server >" + message + "\n")
+#        clsock.send(message + "\n")
+        print "<-RQUEST === [ " + str(message) + " ]"
+#    clsock.send("HTTP/1.0 200 OK\nServer: Test Web\nCache-Control: private\nContent-Type: text/html; charset=UTF-8\nLocation: http://127.0.0.1:1234\nContent-Length: 13\nDate: Thu, 25 Aug 2016 16:48:09 GMT\nConnection: close\n\nHello World!\n")
+   # print "->RESPOND " + str(flag)
 
-    if message[:5] == "/exit":
-        clsock.close()
+        if message[:5] == "/exit":
+            clsock.close()
+            res = 1
+        elif message[:6] == "/trnum":
+            print str( "Number of threads is: " + str( thread_count ) )
+            clsock.send( "threads num is: " + str( thread_count) + "\n" )
+        elif message[:7] == "/lssock":
+            clsock.send( str(all_sockets) )
+        elif message[:5] == "/help":
+            clsock.send( "/exit - Exit\n/trnum - list number of threads\n/lssock - sockets list\n/help - Help"  )
+    else:
         res = 1
-    elif message[:6] == "/trnum":
-        print str( "Number of threads is: " + str( thread_count ) )
-        clsock.send( "threads num is: " + str( thread_count) + "\n" )
-    elif message[:7] == "/lssock":
-        clsock.send( str(all_sockets) )
 
     return res
 
 def cl_socket_thread( thread_name, client_socket, client_address):
     global thread_count
     print "[" + thread_name + "]> Get connection from " + str(client_address) + "\n"
-#    client_socket.send("Thank you for conneting to Test socket server! \n")
- #   client_socket.send("Please enter your input.\n")
+    client_socket.send("Thank you for conneting to Test socket server! \n")
+    client_socket.send("Please enter your input.\n")
     while True:
-        if communication( client_socket, client_address ) == 1:
-            thread_count -= 1
-            break
+        if client_socket:
+            print "THIS IS CLIENT SOCKET"
+            print " = " + str(client_socket)
+            if communication( client_socket, client_address ) == 1:
+                thread_count -= 1
+                break
 
 def open_conn(ip, port, num):
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
